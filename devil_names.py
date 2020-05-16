6c535e09faca49adcba5422ca60f3ba5fbb986ad
guilhermealbm <gui.miranda.13@gmail.com>
2020-05-15 23:40:45 -0300

    devil names local code

---
+++ devil_names.py
@@ -0,0 +1,22 @@
+import random
+
+def main():
+    devil_names = ['Anhangão', 'Aquele', 'Arrenegado', 'Austero', 'Azarape',
+    'Barzabu', 'Beuzebu', 'Bode-Preto' 'Canho', 'Cão', 'Cão-Extremo', 
+    'Cão-miúdo', 'Cão-Tinhoso', 'Capeta', 'Capiroto', 'Caracães', 'Careca', 
+    'Carmulhão', 'Carocho', 'Carujo', 'Coisa-Má', 'Coisa-Ruim', 'Coxo', 'Crespo', 'Cujo', 
+    'Dado', 'Danador', 'Das-Trevas', 'Dê', 'Debo', 'Demo', 'Demonião', 'Demônio', 'Diá', 
+    'Diabo', 'Diabinho', 'Diacho', 'Dianho', 'Dião', 'Diogo', 'Dioguim', 'Dos-Fins',
+    'Drão', 'Duba-Dubá', 'Ele', 'Figura', 'Galhardo', 'Grão-Tinhoso', 'Homem', 'Hermógenes',
+    'Individuo', 'Lúcifer', 'Mafarro', 'Mal-Encarado', 'Maligno', 'Manfarro', 'Morcegão', 
+    'Não-Sei-Que-Diga', 'Oculto', 'O-Muito-Sério', 'O-Que-Não-Existe', 'O-Que-Não-Ri', 
+    'O-Que-Nunca-Fala', 'O-Que-Nunca-Se-Ri', 'Outro', 'Pai-do-Mal', 'Pai-da-Mentira',
+    'Pé-de-Pato', 'Pé-Preto', 'Que-Diga', 'Que-Não-Há', 'Quem-Não-Existe', 'Rapaz',
+    'Romãozinho', 'Roncôlho', 'Satanão', 'Sem-Gracejo', 'Sem-Olho', 'Sempre-Sério', 'Satanás',
+    'Sôuto-Eu', 'Severo-Mor', 'Sujo', 'Tal', 'Temba', 'Tendeiro', 'Tentador', 'Tibes',
+    'Tinhoso', 'Tisnado', 'Tralha', 'Tranjão', 'Tristoho', 'Tunes', 'Xu']
+
+    print(devil_names[random.randrange(len(devil_names))])
+
+if __name__ == '__main__':
+    main()
