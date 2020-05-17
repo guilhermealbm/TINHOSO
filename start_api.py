8e4631e08e7fc5161d5a667603ad59ef7fe24ab5
guilhermealbm <gui.miranda.13@gmail.com>
2020-05-17 19:25:21 -0300

    new flask route - get devil name by index

--- start_api.py
+++ start_api.py
@@ -5,8 +5,16 @@ import devil_names
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return jsonify(devil_names.get_devil_name())
+def get_random_devil_name():
+    return jsonify(devil_names.get_random_devil_name())
+
+@app.route('/devil/<index>')
+def get_devil_name(index):
+    try: 
+        index = int(index)
+        return jsonify(devil_names.get_devil_name(index))
+    except ValueError:
+        return jsonify(index+' is not a possible index for devil names.')
 
 if __name__ == '__main__':
     app.run()
